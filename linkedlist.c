5318f4202fc2068acc34031ff1c8dce04b17f740
Sam Behm <sambehmwork@gmail.com>
2023-06-06 13:01:04 +1000

    added functionality for generate, add, clone, and destroy functions

--- linkedlist.c
+++ linkedlist.c
@@ -1,23 +1,83 @@
 #include "linkedlist.h"
 #include <stdlib.h>
+#include <string.h>
 
 int linklist_generate(LinkedList** list) {
+    LinkedList* llist = malloc(sizeof(LinkedList*));
+    
+    if (llist == NULL) {
+        return LINKEDLIST_MEM_ERROR;
+    }
+    
+    llist->head = NULL;
+    *list = llist;
+    
+    return LINKEDLIST_OK;
+}
+
+int linklist_clone(LinkedList** dstlist, LinkedList* srclist, size_t dataSize) {
+    LinkedList* newlist;
+    int error;
+    
+    if (srclist == NULL) {
+        return LINKEDLIST_INVALID_LIST;
+    }
+
+    error = linklist_generate(&newlist);
+    if (error != LINKEDLIST_OK) {
+        return error;
+    }
+
+    Node* cnode = srclist->head;
+    while (cnode != NULL) {
+        error = linklist_add(cnode->data, dataSize, newlist);
+        
+        if (error != LINKEDLIST_OK) {
+            linklist_destroy(&newlist, &free);
+            return error;
+        }
+        
+        cnode = cnode->next;
+    }
+    
+    *dstlist = newlist;
+    return LINKEDLIST_OK;
+}
+
+int linklist_add(void* data, size_t dataSize, LinkedList* list) {
+    if (list == NULL) {
+        return LINKEDLIST_INVALID_LIST;
+    }
+    
+    Node* newNode, * tail;
+    newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        return LINKEDLIST_MEM_ERROR;
+    }
+
+    newNode->next = NULL;
+    newNode->data = malloc(dataSize);
+    memcpy(newNode->data, data, dataSize);
+
+    tail = list->head;
+    if (tail == NULL) {
+        list->head = newNode;
+        return LINKEDLIST_OK;
+    }
+
+    while(tail->next != NULL) {
+        tail = tail->next;
+    }
+    tail->next = newNode;
+
+    return LINKEDLIST_OK;
+}
+
+int linklist_prepend(void* data, size_t dataSize, LinkedList* list) {
     return 0;
 }
 
-int linklist_clone(LinkedList** dstlist, LinkedList* srclist) {
-    return 0;
-}
-
-int linklist_add(void* data, LinkedList* list) {
-    return 0;
-}
-
-int linklist_prepend(void* data, LinkedList* list) {
-    return 0;
-}
-
-int linklist_insert(int index, void* data, LinkedList* list) {
+int linklist_insert(int index, void* data, size_t dataSize, LinkedList* list) {
     return 0;
 }
 
@@ -29,8 +89,26 @@ int linklist_size(LinkedList* list) {
     return 0;
 }
 
-int linklist_destroy(LinkedList** list) {
-    return 0;
+int linklist_destroy(LinkedList** list, void (*destroyFunction)(void*)) {
+    if (list == NULL || *list == NULL) {
+        return LINKEDLIST_INVALID_LIST;
+    }
+    
+    LinkedList* dlist = *list;
+
+    Node* cnode = dlist->head, * temp;
+    while (cnode != NULL) {
+        temp = cnode->next;
+        (*destroyFunction)(cnode->data);
+        free(cnode);
+        cnode = temp;
+    }
+    temp = NULL;
+
+    free(dlist);
+    dlist = NULL;
+    *list = NULL;
+    return LINKEDLIST_OK;
 }
 
 int linklist_indexOf(void* data, LinkedList* list, int (*compare)(void* a, void* b)) {
